539ddba1231d8f397cc30d2f1c3f88dcd65873a8
Adiel <Adiel@UCPERALTA>
2023-03-02 23:53:21 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblNombreUsuario = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblContrasenia = new System.Windows.Forms.Label();
+            this.lblConfirmarContrasenia = new System.Windows.Forms.Label();
+            this.txtNombreUsuario = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtContrasenia = new System.Windows.Forms.TextBox();
+            this.txtConfirmarContrasenia = new System.Windows.Forms.TextBox();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.lblSemestre = new System.Windows.Forms.Label();
+            this.txtSemestre = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombreUsuario
+            // 
+            this.lblNombreUsuario.AutoSize = true;
+            this.lblNombreUsuario.Location = new System.Drawing.Point(148, 71);
+            this.lblNombreUsuario.Name = "lblNombreUsuario";
+            this.lblNombreUsuario.Size = new System.Drawing.Size(54, 15);
+            this.lblNombreUsuario.TabIndex = 0;
+            this.lblNombreUsuario.Text = "Nombre:";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Location = new System.Drawing.Point(156, 110);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(46, 15);
+            this.lblCorreo.TabIndex = 1;
+            this.lblCorreo.Text = "Correo:";
+            // 
+            // lblContrasenia
+            // 
+            this.lblContrasenia.AutoSize = true;
+            this.lblContrasenia.Location = new System.Drawing.Point(132, 184);
+            this.lblContrasenia.Name = "lblContrasenia";
+            this.lblContrasenia.Size = new System.Drawing.Size(70, 15);
+            this.lblContrasenia.TabIndex = 2;
+            this.lblContrasenia.Text = "Contraseña:";
+            // 
+            // lblConfirmarContrasenia
+            // 
+            this.lblConfirmarContrasenia.AutoSize = true;
+            this.lblConfirmarContrasenia.Location = new System.Drawing.Point(78, 219);
+            this.lblConfirmarContrasenia.Name = "lblConfirmarContrasenia";
+            this.lblConfirmarContrasenia.Size = new System.Drawing.Size(127, 15);
+            this.lblConfirmarContrasenia.TabIndex = 3;
+            this.lblConfirmarContrasenia.Text = "Confirmar Contraseña:";
+            // 
+            // txtNombreUsuario
+            // 
+            this.txtNombreUsuario.Location = new System.Drawing.Point(208, 68);
+            this.txtNombreUsuario.Name = "txtNombreUsuario";
+            this.txtNombreUsuario.Size = new System.Drawing.Size(129, 23);
+            this.txtNombreUsuario.TabIndex = 4;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(208, 107);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(129, 23);
+            this.txtCorreo.TabIndex = 5;
+            // 
+            // txtContrasenia
+            // 
+            this.txtContrasenia.Location = new System.Drawing.Point(208, 181);
+            this.txtContrasenia.Name = "txtContrasenia";
+            this.txtContrasenia.Size = new System.Drawing.Size(129, 23);
+            this.txtContrasenia.TabIndex = 6;
+            // 
+            // txtConfirmarContrasenia
+            // 
+            this.txtConfirmarContrasenia.Location = new System.Drawing.Point(208, 216);
+            this.txtConfirmarContrasenia.Name = "txtConfirmarContrasenia";
+            this.txtConfirmarContrasenia.Size = new System.Drawing.Size(129, 23);
+            this.txtConfirmarContrasenia.TabIndex = 7;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(313, 297);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(91, 23);
+            this.btnRegistro.TabIndex = 8;
+            this.btnRegistro.Text = "Registrarme";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(57, 297);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegresar.TabIndex = 9;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // lblSemestre
+            // 
+            this.lblSemestre.AutoSize = true;
+            this.lblSemestre.Location = new System.Drawing.Point(147, 149);
+            this.lblSemestre.Name = "lblSemestre";
+            this.lblSemestre.Size = new System.Drawing.Size(58, 15);
+            this.lblSemestre.TabIndex = 10;
+            this.lblSemestre.Text = "Semestre:";
+            // 
+            // txtSemestre
+            // 
+            this.txtSemestre.Location = new System.Drawing.Point(208, 146);
+            this.txtSemestre.Name = "txtSemestre";
+            this.txtSemestre.Size = new System.Drawing.Size(129, 23);
+            this.txtSemestre.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(147, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 332);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtSemestre);
+            this.Controls.Add(this.lblSemestre);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.txtConfirmarContrasenia);
+            this.Controls.Add(this.txtContrasenia);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtNombreUsuario);
+            this.Controls.Add(this.lblConfirmarContrasenia);
+            this.Controls.Add(this.lblContrasenia);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblNombreUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "REGISTRATE";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblNombreUsuario;
+        private Label lblCorreo;
+        private Label lblContrasenia;
+        private Label lblConfirmarContrasenia;
+        private TextBox txtNombreUsuario;
+        private TextBox txtCorreo;
+        private TextBox txtContrasenia;
+        private TextBox txtConfirmarContrasenia;
+        private Button btnRegistro;
+        private Button btnRegresar;
+        private Label lblSemestre;
+        private TextBox txtSemestre;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Proyecto/Form1.cs
@@ -0,0 +1,75 @@
+using System.Diagnostics;
+using System.Drawing;
+using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace Proyecto
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Goldenrod;
+           
+
+        }
+
+
+        private void btnRegistro_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = txtNombreUsuario.Text;
+            string correo = txtCorreo.Text;
+            string semestre = txtSemestre.Text;
+            string contrasenia = txtContrasenia.Text;
+            string confirmarContrasenia = txtConfirmarContrasenia.Text;
+
+            // Validar que los campos no estén vacíos
+            if (string.IsNullOrWhiteSpace(nombreUsuario) ||
+                string.IsNullOrWhiteSpace(correo) ||
+                string.IsNullOrWhiteSpace(semestre) ||
+                string.IsNullOrWhiteSpace(contrasenia) ||
+                string.IsNullOrWhiteSpace(confirmarContrasenia))
+            {
+                MessageBox.Show("Por favor complete todos los campos.");
+                return;
+            }
+
+            // Validar que las contraseñas coincidan
+            if (contrasenia != confirmarContrasenia)
+            {
+                MessageBox.Show("Las contraseñas no coinciden.");
+                return;
+            }
+
+            // Validar que el correo tenga un formato válido
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            if (!regex.IsMatch(correo))
+            {
+                MessageBox.Show("Ingrese una dirección de correo electrónico válida.");
+                return;
+            }
+
+            // TODO: Agregar validaciones adicionales según tus necesidades, como verificar si el nombre de usuario ya está en uso
+
+            // Si llegamos hasta aquí, el registro fue exitoso
+            MessageBox.Show("Registro exitoso.");
+
+            // Cambiar a la ventana de inicio de sesión
+            this.Hide();
+            Form ventanaInicioSesion = new Form();
+            // TODO: Agregar controles y lógica para la ventana de inicio de sesión según tus necesidades
+            ventanaInicioSesion.Show();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            this.Hide(); //ocultar la ventana actual
+            Form ventanaInicioSesion = new Form(); //crear una instancia de la ventana anterior
+            ventanaInicioSesion.Show(); //mostrar la ventana anterior
+        }
+
+    }
+}
